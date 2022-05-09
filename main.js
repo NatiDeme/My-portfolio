18fa3d617b294b55809a74da2915093dc9c2d93b
RICH <adukyerer@gmail.com>
2022-05-09 15:19:34 +0000

    add javascript DOM

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+let isClicked = false;
+
+function display() {
+  if (!isClicked) {
+    document.getElementById('mnc').style.display = 'block';
+    document.getElementById('mnha').style.visibility = 'hidden';
+    document.getElementById('menu').src = 'images/close.png';
+    document.getElementById('menu').style.height = '1.8rem';
+
+    isClicked = true;
+  } else {
+    document.getElementById('mnc').style.display = 'none';
+    document.getElementById('mnha').style.visibility = 'visible';
+    document.getElementById('menu').src = 'images/more.png';
+
+    isClicked = false;
+  }
+}
+
+function menuClicked() {
+  display();
+}
 
